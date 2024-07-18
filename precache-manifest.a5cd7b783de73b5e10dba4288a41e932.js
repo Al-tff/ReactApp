44369471c057d40399a3fa96b16cdb82eff9a1ae
althff <althafmalthaf2803@gmail.com>
2024-07-18 12:05:29 +0530

    Updates

---
+++ precache-manifest.a5cd7b783de73b5e10dba4288a41e932.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52320be7eaa311a39ec23bae1c5b878d",
+    "url": "/ReactApp/index.html"
+  },
+  {
+    "revision": "4f8c61cb1fa02ec8be98",
+    "url": "/ReactApp/static/css/main.23fbebcc.chunk.css"
+  },
+  {
+    "revision": "2c97e1e1f10898f872cb",
+    "url": "/ReactApp/static/js/2.5a17e8bd.chunk.js"
+  },
+  {
+    "revision": "de6bb9b182cb4883b744809bb1ef7215",
+    "url": "/ReactApp/static/js/2.5a17e8bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4df83d3051840386dff0",
+    "url": "/ReactApp/static/js/3.76f02b70.chunk.js"
+  },
+  {
+    "revision": "4f8c61cb1fa02ec8be98",
+    "url": "/ReactApp/static/js/main.42dd515e.chunk.js"
+  },
+  {
+    "revision": "6f30552d931f60651a3d",
+    "url": "/ReactApp/static/js/runtime-main.f97635d8.js"
+  },
+  {
+    "revision": "cea3c6cc75fd4b206f1c6391a04f0984",
+    "url": "/ReactApp/static/media/plant5.cea3c6cc.jpeg"
+  }
+]);
